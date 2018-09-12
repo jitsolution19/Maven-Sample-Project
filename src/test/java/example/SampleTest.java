587811695bd48d4575be366ad4149372f4b36cc4
jitsolution19 <jitsolution19@gmail.com>
2018-09-13 01:06:34 +0530

    update

---
+++ src/test/java/example/SampleTest.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	private WebDriver driver;
+
+	@BeforeTest
+	public void beforetest() {
+		System.out.println("Launch Application");
+		System.setProperty("webdriver.chrome.driver", "B:\\Automation\\software\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+	}
+
+	@Test
+	public void temp() {
+		driver.get("https://housing.com/");
+		driver.manage().window().maximize();
+	}
+
+	@AfterTest
+	public void aftertest() {
+		driver.quit();
+	}
+}
